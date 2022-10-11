001508624305974ea3ac7f276ed4eff318ccf4ad
imransaglam <imransaglam9@gmail.com>
2022-10-11 11:19:49 +0300

    sayfalar oluşturuldu

---
+++ lib/Restaurant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  const RestaurantScreen({super.key});
+
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
