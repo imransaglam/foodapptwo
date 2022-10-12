c2d0dae43fe1e160a5cab7169e7d9287138371a8
imransaglam <imransaglam9@gmail.com>
2022-10-12 09:59:02 +0300

    spalsh screen sayfası tamamlandı

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:foodapptwo/widgets/backgroundimage.dart';
+import 'package:foodapptwo/widgets/createAccountText.dart';
+import 'package:foodapptwo/widgets/loginbutton.dart';
+import 'package:foodapptwo/widgets/logintext.dart';
+import 'package:foodapptwo/widgets/textAccount.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,6 +17,17 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor:  Color(0xff010E16),
+      body: Column(
+        children: [
+          BackgroundImage(),
+          LoginText(),
+          LoginButton(),
+          textAccount(),
+          createAccountText(),
+        ],
+      ),
+    );
   }
 }
